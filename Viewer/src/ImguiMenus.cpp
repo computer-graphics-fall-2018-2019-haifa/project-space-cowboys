8845ba59c4b6bc7c4f1ef46ee8ea78b63de4117d
Tal-katz <tkatz08@campus.haifa.ac.il>
2018-11-19 21:31:43 +0200

    some menues

--- Viewer/src/ImguiMenus.cpp
+++ Viewer/src/ImguiMenus.cpp
@@ -253,28 +253,49 @@ void DrawImguiMenus(ImGuiIO& io, Scene& scene)
 		static float angley = 0.0f;
 		static float anglez = 0.0f;
 		static int e = 0;
+		static float vec4f[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
 
 		ImGui::RadioButton("Scale", &e, 0); ImGui::SameLine();
 		ImGui::RadioButton("Move", &e, 1); ImGui::SameLine();
 		ImGui::RadioButton("Rotate", &e, 2);
-		
+		if (e == 0) {
 
-		ImGui::SliderAngle("X", &anglex);
-		if (ImGui::Button("reset"))
-		{
-			anglex = 0.0f;
+			ImGui::DragFloat3("drag float3", vec4f, 0.1f, -10.0f, 10.0f);
+
 		}
-		ImGui::SliderAngle("Y", &angley); 
-		if (ImGui::Button("reset"))
-		{
-			angley = 0.0f;
+		if (e == 1) {
+			
+			ImGui::DragFloat3("drag float3", vec4f, 0.1f, -10.0f, 10.0f);
+			
 		}
-		ImGui::SliderAngle("Z", &anglez);
-		if (ImGui::Button("reset"))
-		{
-			anglez = 0.0f;
+		if (e == 2) {
+			ImGui::PushStyleColor(ImGuiCol_FrameBg, { 1.0f, 0.0f, 0.0f, 0.8 });
+			ImGui::Text("rotate x"); ImGui::SameLine();
+			ImGui::SliderAngle("x", &anglex); ImGui::SameLine();
+			
+			if (ImGui::Button("reset x"))
+			{
+				anglex = 0.0f;
+			}
+			ImGui::PopStyleColor(1);
+			ImGui::PushStyleColor(ImGuiCol_FrameBg, { 0.0f, 1.0f, 0.0f, 0.8 });
+			ImGui::Text("rotate y"); ImGui::SameLine();
+			ImGui::SliderAngle("y", &angley); ImGui::SameLine();
+			ImGui::PopStyleColor(1);
+			if (ImGui::Button("reset y"))
+			{
+				angley = 0.0f;
+			}
+			ImGui::PushStyleColor(ImGuiCol_FrameBg, { 0.0f, 0.0f, 1.0f, 0.8 });
+			ImGui::Text("rotate z"); ImGui::SameLine();
+			ImGui::SliderAngle("z", &anglez); ImGui::SameLine();
+			ImGui::PopStyleColor(1);
+			if (ImGui::Button("reset z"))
+			{
+				anglez = 0.0f;
+			}
 		}
-		ImGui::Text("tt");
+		ImGui::Text("");
 		
 		
 		ImGui::End();
